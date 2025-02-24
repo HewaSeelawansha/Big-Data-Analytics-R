ebfa281056861522171919afb50273b0eceeaac1
Hewa Seelawansha <135198286+HewaSeelawansha@users.noreply.github.com>
2025-02-25 01:08:16 +0530

    Temperature against rainfall distribution of clusters

--- K-means - Climate Dataset - R/K-means climate dataset.R
+++ K-means - Climate Dataset - R/K-means climate dataset.R
@@ -15,3 +15,29 @@ ggplot(data, aes(x= Temperature, y= Rainfall, color=ClimateZone))+
        x="Temperature",
        y="Rainfall")+
   theme_minimal()
+
+#implementation cluster model using k-means
+
+#let's remove country column
+model_data <- data[,c("Temperature", "Rainfall")]
+head(model_data)
+
+#k-means model
+set.seed(100)
+results <- kmeans(model_data, centers = 4)
+
+#adding labels to clusters
+data$cluster <- as.factor(results$cluster)
+
+#to show the cluster numbers use this "data$cluster"
+
+#drawing cluster results
+ggplot(data, aes(x= Temperature, y= Rainfall, color=cluster))+
+  stat_ellipse(aes(fill=cluster), geom = "polygon", alpha=0.4) + 
+  geom_point(size=3, alpha=0.8)+
+  labs(title= "Temperature against rainfall distribution of clusters",
+       x="Temperature",
+       y="Rainfall")+
+  theme_minimal()
+
+table(data$ClimateZone, data$cluster)
