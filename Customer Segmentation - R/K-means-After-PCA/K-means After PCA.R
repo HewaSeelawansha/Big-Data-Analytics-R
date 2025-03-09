85bda414f08e2897ce90b4275199980875bb846f
Hewa Seelawansha <135198286+HewaSeelawansha@users.noreply.github.com>
2025-03-09 22:38:31 +0530

    pca

--- Customer Segmentation - R/K-means-After-PCA/K-means After PCA.R
+++ Customer Segmentation - R/K-means-After-PCA/K-means After PCA.R
@@ -8,3 +8,10 @@ data <- read.csv("customer_data_pca.csv")
 scaled_data <- scale(data)
 head(scaled_data)
 
+#calculating PCA with eigenvalues and eigenvectors
+pca_model <- prcomp(scaled_data, center=TRUE, scale=TRUE)
+pca_model
+
+#transforming data set into principal components
+pca_transformed_data <- as.data.frame(pca_model$x)
+
