cde5f0ed4240e42b4eb0a77f9d42f568d41abc0f
Hewa Seelawansha <135198286+HewaSeelawansha@users.noreply.github.com>
2025-03-15 12:44:05 +0530

    annual income against spending score

--- Enhanced Customer Segmentation - R/K-means-Before-PCA/K-means Customer Segmentation.R
+++ Enhanced Customer Segmentation - R/K-means-Before-PCA/K-means Customer Segmentation.R
@@ -35,3 +35,13 @@ ggplot(data, aes(x= Age, y= Spending_Score, color=cluster))+
        y="Spending_Score")+
   theme_minimal()
 
+#Annual_Income against Spending_Score
+ggplot(data, aes(x= Annual_Income, y= Spending_Score, color=cluster))+
+  stat_ellipse(aes(fill=cluster), geom = "polygon", alpha=0.4) + 
+  geom_point(size=3, alpha=0.8)+
+  labs(title= "Annual_Income against Spending_Score distribution of clusters",
+       x="Annual_Income",
+       y="Spending_Score")+
+  theme_minimal()
+
+
