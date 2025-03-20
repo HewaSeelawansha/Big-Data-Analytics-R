81bb91365b9a438bf367cc70db0c8d0024816dd5
Hewa Seelawansha <135198286+HewaSeelawansha@users.noreply.github.com>
2025-03-20 15:14:20 +0530

    pca file

---
+++ Enhanced Customer Segmentation - R/PCA/PCA Large Customer.R
@@ -0,0 +1,29 @@
+data <- read.csv("D://NSBM//3rd year 2nd semester//Big Data Analysis//Lab//Lab//Week-03 Submission DLE - 23-02-2025//large_pca_data.csv")
+
+library(ggplot2)
+#install.packages("GGally")
+library(GGally)
+library(dplyr)
+library(factoextra)
+
+#Standardization
+scaled_data <- scale(data)
+head(scaled_data)
+
+#calculating PCA with Eigenvalues and Eigenvectors
+pca_model <- prcomp(scaled_data, center=TRUE, scale=TRUE)
+pca_model
+summary(pca_model)
+
+#screen Plot
+fviz_eig(pca_model, addlabels = TRUE, ylim = c(0,100))
+
+#transforming data set into principal components
+pca_transformed_data <- as.data.frame(pca_model$x)
+head(pca_transformed_data)
+
+ggpairs(pca_transformed_data[, c("PC1", "PC2", "PC3", "PC4")],
+        aes(color = I("blue"), alpha = 0.6)) +
+  theme_minimal() +
+  ggtitle("Pairwise Plot of the First 4 Principal Components")
+
