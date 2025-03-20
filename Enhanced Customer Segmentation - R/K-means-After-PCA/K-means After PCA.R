0d03a94cb6eeb49b94b33ea3fddbf2ef5c983fb3
Hewa Seelawansha <135198286+HewaSeelawansha@users.noreply.github.com>
2025-03-21 02:31:04 +0530

    hull

--- Enhanced Customer Segmentation - R/K-means-After-PCA/K-means After PCA.R
+++ Enhanced Customer Segmentation - R/K-means-After-PCA/K-means After PCA.R
@@ -33,3 +33,33 @@ ggpairs(
   theme_minimal() +
   ggtitle("Pairwise Plot of the First 4 Principal Components with Clusters")
 
+#install.packages("cluster")
+library(cluster)
+
+# Function to Compute Convex Hull for Given X and Y
+compute_hull <- function(df, x, y) {
+  df[chull(df[[x]], df[[y]]), ]
+}
+
+# Generate Convex Hull Data for All Pairwise Combinations
+hull_data <- bind_rows(
+  lapply(
+    c("PC1", "PC2", "PC3", "PC4"),
+    function(x) {
+      lapply(
+        c("PC1", "PC2", "PC3", "PC4"),
+        function(y) {
+          if (x != y) {
+            pca_transformed_data %>%
+              group_by(cluster) %>%
+              group_split() %>%
+              lapply(compute_hull, x = x, y = y) %>%
+              bind_rows() %>%
+              mutate(x_col = x, y_col = y)
+          }
+        }
+      )
+    }
+  ) %>% unlist(recursive = FALSE)
+)
+
