16b1bde14b248b32eba16d94369bac26529801b3
Hewa Seelawansha <135198286+HewaSeelawansha@users.noreply.github.com>
2025-03-20 15:10:53 +0530

    libraries

--- Enhanced Customer Segmentation - R/PCA/Principal Component Analysis.R
+++ Enhanced Customer Segmentation - R/PCA/Principal Component Analysis.R
@@ -1,2 +1,8 @@
 data <- read.csv("D://NSBM//3rd year 2nd semester//Big Data Analysis//Lab//Lab//Week-03 Submission DLE - 23-02-2025//large_pca_data.csv")
 
+library(ggplot2)
+#install.packages("GGally")
+library(GGally)
+library(dplyr)
+library(factoextra)
+
