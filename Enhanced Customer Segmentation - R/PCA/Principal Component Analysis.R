3883e603ecfe8eeb241fedc7e7471893428afb5d
Hewa Seelawansha <135198286+HewaSeelawansha@users.noreply.github.com>
2025-03-21 02:32:35 +0530

    transforming data set into principal components

--- Enhanced Customer Segmentation - R/PCA/Principal Component Analysis.R
+++ Enhanced Customer Segmentation - R/PCA/Principal Component Analysis.R
@@ -1,4 +1,4 @@
-data <- read.csv("D://NSBM//3rd year 2nd semester//Big Data Analysis//Lab//Lab//Week-03 Submission DLE - 23-02-2025//large_pca_data.csv")
+data <- read.csv("large_pca_data.csv")
 
 library(ggplot2)
 #install.packages("GGally")
